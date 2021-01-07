d98a1261dd2633c44ebb308110f0d3c6b7bfa882
Long <hokkimlong@outlook.com>
2021-01-07 12:52:23 +0700

    a

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,8 @@ export default function Home({ data }) {
       {/* <Head>
           <title>{`${data[0]?.name} store`}</title>
         </Head> */}
-      <MessengerCustomerChat appId="" pageId="101433091549607" />
+      <h1>{data.subdomain}</h1>
+      <MessengerCustomerChat appId="" pageId={data.pageId} />
       {/* <div className="container">
           {data[0]?.products.map((product) => (
             <h1>{product.name}</h1>
@@ -21,24 +22,39 @@ export default function Home({ data }) {
   // }
 }
 
-// export async function getServerSideProps(context) {
-//   const host = context.req.headers.host.split(".");
-//   const subdomain = host.length === 2 ? host[0] : "";
-//   console.log(subdomain);
-//   let data = null;
-//   try {
-//     const res = await axios.get(
-//       `http://localhost:1337/stores?name=${subdomain}`
-//     );
-//     data = res.data;
-//   } catch (err) {
-//     console.log(err);
-//   }
-//   if (data.length === 0) {
-//     data = { message: "No Store Found" };
-//   }
-//   console.log(data);
-//   return {
-//     props: { data }, // will be passed to the page component as props
-//   };
-// }
+export async function getServerSideProps(context) {
+  const host = context.req.headers.host.split(".");
+  const subdomain = host.length === 2 ? host[0] : "";
+  let data = null;
+  if (subdomain === "bei") {
+    data = {
+      pageId: "101433091549607",
+      subdomain: "bei",
+    };
+  } else if (subdomain === "gigi") {
+    data = {
+      pageId: "101844401876723",
+      subdomain: "gigi",
+    };
+  }
+  return {
+    props: { data },
+  };
+  // console.log(subdomain);
+  // let data = null;
+  // try {
+  //   const res = await axios.get(
+  //     `http://localhost:1337/stores?name=${subdomain}`
+  //   );
+  //   data = res.data;
+  // } catch (err) {
+  //   console.log(err);
+  // }
+  // if (data.length === 0) {
+  //   data = { message: "No Store Found" };
+  // }
+  // console.log(data);
+  // return {
+  //   props: { data }, // will be passed to the page component as props
+  // };
+}
